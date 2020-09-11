31c36929a37e2006f6f1a523fc731fcaa29fb4ac
Simplex Rimor <simplex.rimor@gmail.com>
2020-09-11 16:25:24 -0400

    Blink

--- digital_output_group.go
+++ digital_output_group.go
@@ -1,6 +1,44 @@
 package ganglia
 
+import (
+	"time"
+)
+
 type DigitalOutputGroup []DigitalOutput
+
+func (group DigitalOutputGroup) Blink(value uint, on, off time.Duration) Stopper {
+	done := make(chan bool)
+	go group.blink(value, on, off, done)
+	return func() { close(done) }
+}
+
+func (group DigitalOutputGroup) blink(value uint, on, off time.Duration, done <-chan bool) {
+	active := true
+	timer := time.After(on)
+
+	for running := true; running; {
+		var out uint
+
+		if active {
+			out = value
+		}
+
+		group.Write(out)
+
+		select {
+		case <-done:
+			running = false
+		case <-timer:
+			active = !active
+
+			if active {
+				timer = time.After(on)
+			} else {
+				timer = time.After(off)
+			}
+		}
+	}
+}
 
 func (group DigitalOutputGroup) Write(value uint) {
 	count := len(group)
@@ -13,3 +51,5 @@ func (group DigitalOutputGroup) Write(value uint) {
 		}
 	}
 }
+
+type Stopper func()
--- digital_output_group_test.go
+++ digital_output_group_test.go
@@ -64,3 +64,36 @@ func TestDigitalOutputGroup(t *testing.T) {
 		t.Error("expected high")
 	}
 }
+
+func TestDigitalOutputGroupBlink(t *testing.T) {
+	var results []DigitalValue
+	channel := make(chan DigitalValue, 1)
+	group := DigitalOutputGroup([]DigitalOutput{channel})
+
+	go func() {
+		for value := range channel {
+			results = append(results, value)
+		}
+	}()
+
+	stop := group.Blink(1, 10*time.Millisecond, 20*time.Millisecond)
+	time.Sleep(200 * time.Millisecond)
+	stop()
+	close(channel)
+
+	if count := len(results); count == 0 {
+		t.Error("expect results but got 0")
+	}
+
+	level := High
+
+	for index, result := range results {
+		t.Logf("index %d level %t", index, result)
+
+		if result != level {
+			t.Errorf("expected %d to be %t but got %t", index, level, result)
+		}
+
+		level = !level
+	}
+}
