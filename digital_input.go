11f112d9a7f4c3524bd6f87b0199c5a1aacc21f8
Simplex Rimor <simplex.rimor@gmail.com>
2020-11-28 16:14:17 -0500

    Stabilize digital input

--- digital_input.go
+++ digital_input.go
@@ -44,6 +44,10 @@ func (input DigitalInput) debounce(debounced chan<- *DigitalEvent, duration time
 			)
 		}
 	}
+
+	if timer != nil {
+		timer.Stop()
+	}
 }
 
 func (input DigitalInput) Invert() DigitalInput {
@@ -63,6 +67,39 @@ func (input DigitalInput) invert(inverted chan<- *DigitalEvent) {
 func (input DigitalInput) Read() *DigitalEvent {
 	value := <-input
 	return value
+}
+
+func (input DigitalInput) Stabilize(duration time.Duration) DigitalInput {
+	stabilized := make(chan *DigitalEvent)
+	go input.stabilize(stabilized, duration)
+	return stabilized
+}
+
+func (input DigitalInput) stabilize(stabilized chan<- *DigitalEvent, duration time.Duration) {
+	var last *DigitalEvent
+	var timer *time.Timer
+	defer close(stabilized)
+
+	for event := range input {
+		if last == nil || last.Value != event.Value {
+			if timer != nil {
+				timer.Stop()
+			}
+
+			last = event
+
+			timer = time.AfterFunc(
+				duration,
+				func() {
+					stabilized <- event
+				},
+			)
+		}
+	}
+
+	if timer != nil {
+		timer.Stop()
+	}
 }
 
 type DigitalValue bool
